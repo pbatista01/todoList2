c734b391ac6620332fb29663dea3da4f6564dece
Estefanie Gisell Peña Batista <estefanie.pena.b@gmail.com>
2024-02-22 19:49:56 -0500

    Added night mode and effect on delete item from the list

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const btnAdd = document.querySelector(".btn-add");
 const todoList = document.querySelector(".todo-list");
+const modeIcon = document.querySelector(".mode-icon");
 
 btnAdd.addEventListener("click", addItemToList);
-
+modeIcon.addEventListener("click", toggleMode);
 
 function addItemToList(){
   const itemContainer = document.createElement("div")
@@ -14,8 +15,18 @@ function addItemToList(){
   const input = document.querySelector(".list-item");
   
   const btnEliminar = document.createElement("a");
-  btnEliminar.textContent = "X";
+  
   btnEliminar.classList.add("btn-remove");
+  
+  const iconTrash = document.createElement("i");
+  
+  iconTrash.classList.add("fa-solid");
+  
+  iconTrash.classList.add("fa-trash");
+  
+  
+  btnEliminar.appendChild(iconTrash);
+  
   btnEliminar.onclick = function() {
         eraseATodo(item, btnEliminar);
       };
@@ -35,7 +46,25 @@ function addItemToList(){
 
 function eraseATodo(item, btnEliminar){
   if(item && item.parentNode && btnEliminar && btnEliminar.parentNode){
-        item.parentNode.removeChild(item);
-        btnEliminar.parentNode.removeChild(btnEliminar);
+         item.classList.add("removed-item");
+         btnEliminar.classList.add("removed-item");
+    setTimeout(() => {
+      item.parentNode.removeChild(item); 
+      btnEliminar.parentNode.removeChild(btnEliminar);
+    },1000);   
+        
   }
  }
+
+function toggleMode(modeIcon){
+  const container = document.querySelector(".container");
+  const body = document.body;
+  const button = document.querySelector("button");
+  container.classList.toggle("light-mode");
+  body.classList.toggle("light-mode");
+  modeIcon.className ="fa";
+  
+  modeIcon.classList.toggle("fa-sun");
+  button.clasasList.toggle("light-mode");
+}
+
